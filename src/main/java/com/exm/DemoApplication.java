5b0dd2a21fae9386442e1d1171410d141278a74f
yyy <tt@t.com>
2019-03-22 12:08:45 +0800

    update

--- src/main/java/com/exm/DemoApplication.java
+++ src/main/java/com/exm/DemoApplication.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.exm")
 @MapperScan("com.exm.business.mapper")
-public class ShardingDemoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShardingDemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("=====================================");
 		System.out.println("===== 系统启动成功 =====");
 	}
